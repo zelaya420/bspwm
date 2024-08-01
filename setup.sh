86b0d5ebc34b968fedc9a5db7859ece363beabf8
zelaya420 <95180196+zelaya420@users.noreply.github.com>
2024-08-01 09:49:21 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,7 +53,7 @@ else
 	sleep 1
 	echo -e "\n\n${blueColour}[*] Installing necessary packages for the environment...\n${endColour}"
 	sleep 2
-	sudo apt install -y kitty rofi feh xclip ranger i3lock-fancy scrot scrub wmname imagemagick cmatrix htop neofetch python3-pip procps tty-clock fzf lsd bat pamixer flameshot playerctl
+	sudo apt install -y kitty rofi feh xclip ranger betterlockscreen  scrot scrub wmname imagemagick cmatrix htop neofetch python3-pip procps tty-clock fzf lsd bat pamixer flameshot playerctl
 	if [ $? != 0 ] && [ $? != 130 ]; then
 		echo -e "\n${redColour}[-] Failed to install some packages!\n${endColour}"
 		exit 1
